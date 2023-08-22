a1d5485c9f39451d823fa9724a2577b9729ba30e
ximas <45686675+ximas@users.noreply.github.com>
2023-08-22 22:11:05 +0100

    Update test pkg

--- TEST_PKG.sql
+++ TEST_PKG.sql
@@ -1,14 +1,17 @@
 
   CREATE OR REPLACE EDITIONABLE PACKAGE "WKSP_SAMITEST"."TEST_PKG" is
-
+	-- comment
 	procedure test_proc;
 	
 end;
 CREATE OR REPLACE EDITIONABLE PACKAGE BODY "WKSP_SAMITEST"."TEST_PKG" is
 
 	procedure test_proc is
+		l_num number;
 	begin
 		null;
+		-- change
+		l_num := 1;
 	end;
 
 end;
