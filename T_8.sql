5709a58cc942e314dd2efb3744be755fe2da6435
ximas <45686675+ximas@users.noreply.github.com>
2023-08-22 21:59:30 +0100

    Added type of 8 values (2)

---
+++ T_8.sql
@@ -0,0 +1,11 @@
+
+  CREATE OR REPLACE EDITIONABLE TYPE "WKSP_SAMITEST"."T_8" as object (
+	var1 varchar2(100)
+	, var2 varchar2(100)
+	, var3 varchar2(100)
+	, num1 number
+	, num2 number
+	, num3 number
+	, dat1 date
+	, dat2 date
+)
